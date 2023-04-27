e998e282a75ecc75b4e52671ae14d0bc4a1c109e
necklace17 <niklas.scholz@outlook.com>
2023-04-27 22:05:52 +0700

    initial commit

---
+++ catalog-info-service/src/test/java/com/necklace/cataloginfoservice/CatalogInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.necklace.cataloginfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
