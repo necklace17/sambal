74a8ad6a23d866945953ba512cdf71ef8db0d7fb
necklace17 <niklas.scholz@outlook.com>
2023-04-29 10:44:14 +0700

    created module

---
+++ accommodation-review-service/src/test/java/com/necklace/accommodationreviewservice/AccommodationReviewServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.necklace.accommodationreviewservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccommodationReviewServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
